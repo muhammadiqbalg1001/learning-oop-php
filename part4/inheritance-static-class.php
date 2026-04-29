066c4c58e0564ba2123671290973e859aebe8f8d
muhammadiqbal.g1001 <muhammadiqbal.g1001@gmail.com>
2026-04-29 16:15:45 +0700

    learning inheritance static class

---
+++ part4/inheritance-static-class.php
@@ -0,0 +1,46 @@
+<?php
+
+class Student
+{
+    public static $grades = ['SD', 'SMP', 'SMA'];
+    private static $totalStudents = 0;
+
+    public static function motto()
+    {
+        return 'Learn PHP OOP';
+    }
+
+    public static function count()
+    {
+        return self::$totalStudents;
+    }
+
+    public static function addStudent()
+    {
+        return self::$totalStudents++;
+    }
+}
+
+echo Student::$grades[2] . "<br/>";
+echo Student::motto() . "<br/>";
+
+echo Student::count() ."<br/>";
+Student::addStudent();
+echo Student::count() ."<br/>";
+
+class PartTimeStudent extends Student
+{
+}
+
+echo PartTimeStudent::$grades[1] . "<br/>";
+echo PartTimeStudent::motto() . "<br/>";
+
+PartTimeStudent::$grades[] = 'Alumni';
+echo implode(', ', Student::$grades);
+
+Student::addStudent();
+Student::addStudent();
+PartTimeStudent::addStudent();
+
+echo Student::count() . '<br/>';
+echo PartTimeStudent::count() . '<br/>';
